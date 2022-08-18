2b2f8fe0f22703df8a34ff9693d7035b91d636ea
shahmeerali74 <111519383+shahmeerali74@users.noreply.github.com>
2022-08-18 20:39:09 +0500

    initialize

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import { useState } from 'react';
+import './App.css';
+import Navbar from './component/Navbar';
+import Home from './component/Home';
+import About from './component/About';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  const [firstColor,setFirstColor]=useState({
+    backgroundColor:"#072423",
+    color:"red",
+  });
+
+  const[darkmode,setdarkmode]=useState("enable dark mode");
+
+
+
+const[Mode,setMode]=useState("");
+ const handleModeChange=()=>{
+  console.log("helo i am running");
+  if(Mode==='dark'){
+    {
+      setMode("light");
+      document.body.style.backgroundColor="white";
+      document.body.style.color="black";
+      setdarkmode("Enable light mode");
+      setFirstColor({
+        backgroundColor:"#072423",
+        color:"red",
+      })
+      
+    }
+  }
+    else{
+      setMode("dark");
+      document.body.style.backgroundColor="grey";
+      document.body.style.color="green";
+      setdarkmode("Enable dark mode");
+      setFirstColor({
+        backgroundColor:"brown",
+        color:"pink",
+      })
+      
+    }
+
+ }
+
+
+
+
+
+
+
+  return (
+    <>
+    <Router>
+    <Navbar mode={Mode} handleModeChange={handleModeChange} darkmode={darkmode}/>
+
+      {/* <Textutlities heading="Enter Your Text Down Here" txtvalue="Enter text here" color={firstColor} /> */}
+  <div className="container">
+    <Switch>
+
+          <Route exact path="/about">
+              <About/>
+          </Route>
+
+          <Route exact path="/home">
+              <Home/>
+          </Route>
+   
+    </Switch>
+    </div>
+    </Router>
+    </>
+  );
+  }
+
+
+export default App;
---
+++ src/component/Home.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { useState } from 'react'
+
+
+export default function Home(props) {
+const ToUpperCaseHandle=()=>{
+  const newText=text.toLocaleUpperCase();
+    setText(newText);
+    console.log(newText);
+ }
+ const ToCopyHandle=()=>{
+  let copyText= document.getElementById("mytextarea");
+  copyText.select();
+  navigator.clipboard.writeText(copyText.value);
+ }
+
+ const ToLowerCaseHandle=()=>{
+    const newText=text.toLocaleLowerCase();
+    setText(newText)
+ }
+    let onChangeHandler=(event)=>{
+        setText(event.target.value)
+    }
+
+    const[text,setText]=useState("")
+  return (
+    
+    <>
+      <div className='container'>
+          <h1 className="mx-4 mt-4">{props.heading}</h1>
+          <textarea className="form-control" value={text} style={props.color} id="mytextarea" onChange={onChangeHandler} rows="8"></textarea>
+          <button className='btn btn-primary my-4' onClick={ToUpperCaseHandle}>To UpperCase</button>
+          <button className='btn btn-primary mx-4' onClick={ToLowerCaseHandle}>To LowerCase</button>
+          <button className='btn btn-primary mx-4' onClick={ToCopyHandle}>To Copy Text</button>
+          
+
+      </div>
+
+      <div className="container">
+        <h1>Preview of written Text</h1>
+        <p>{text}</p>
+      </div>
+
+      <div className="container">
+        <h1>Words per Minutes</h1>
+        <p>{text.split(" ").length} words</p>
+        <p>{text.length} characters</p>
+        <p></p>
+      </div>
+    </>
+  )
+}
