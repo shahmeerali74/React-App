759874b0de468a76a46f6e4d8f61c1fcde39ce8a
shahmeer ali <syed.sas74@gmail.com>
2023-11-26 05:43:05 -0800

    ading slash
    adding slash

--- docker_push_file.sh
+++ docker_push_file.sh
@@ -6,7 +6,7 @@ dockerhubpass=$2
 docker login -u $dockerhubuser -p $dockerhubpass
 
 # Tag the Docker image with the version (e.g., $BUILD_NUMBER)
-docker tag $imageName:latest $imageName:$BUILD_NUMBER
+docker tag $imageName:latest $imageName/:$BUILD_NUMBER
 
 # Push the tagged image to Docker Hub
-docker push $imageName:$BUILD_NUMBER
+docker push $imageName/:$BUILD_NUMBER
