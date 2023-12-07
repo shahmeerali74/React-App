7ebe1281ed5d4dcd5c364f595428b139bee4c240
shahmeer ali <syed.sas74@gmail.com>
2023-12-07 18:48:27 +0500

    new tag
    adding new tag of image to deploy to docker hub

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ hostport=$1
 appport=$2
 cont_name=$3
 reponame=$4
-docker run -d -p $hostport:$appport --name fc --rm shahmeerali/$imageName:$BUILD_NUMBER
+docker run -d -p $hostport:$appport --name fc --rm shahmeerali/$imageName:$imageName$BUILD_NUMBER
